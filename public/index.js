45e768a0c1f9caab55a8c8ffba6b9f2c65dfc3d5
Andres Jaan Tack <ajtack@twilio.com>
2016-09-29 00:29:07 +0300

    Complete demo with free-drawn lines and moving in groups.

--- public/index.js
+++ public/index.js
@@ -1,195 +1,224 @@
 $(function () {
-  //We'll use message to tell the user what's happening
-  var $message = $('#message');
-
-  //Get handle to the game board buttons
-  var $buttons = $('#board .board-row button');
-
-  //Our interface to the Sync service
-  var syncClient;
-
-  //We're going to use a single Sync document, our simplest
-  //synchronisation primitive, for this demo
-  var syncDoc;
-
-  fabric.Canvas.prototype.getItemByName = function(name) {
-    var object = null,
-        objects = this.getObjects();
-
-    for (var i = 0, len = this.size(); i < len; i++) {
-      if (objects[i].id && objects[i].id === name) {
-        object = objects[i];
-        break;
-      }
-    }
-
-    return object;
-  };
-
   var canvas = new fabric.Canvas('theWhiteboard');
-  canvas.setHeight(500);
-  canvas.setWidth(800);
+  canvas.setHeight(470);
+  canvas.setWidth(770);
+  attachLoggingToCanvas(canvas);
+  attachPencilButtonToCanvas(canvas);
 
-  canvas.on('object:added', function(evt) {
-      console.log(evt)
+  // Every Sync app needs a backend to provision a token. The Sync client uses this token
+  // to interact directly with the Twilio Sync backend from the browser.
+  //
+  $.getJSON('/token', (tokenResponse) => {
+
+    // Initialize the Sync client
+    //
+    const syncClient = new Twilio.Sync.Client(tokenResponse.token);
+    $('#message').html('Sync initialized!');
+
+    // Open a Sync Map describing this whiteboard, page through anything
+    // that's already there.
+    //
+    syncClient.map('myGreatWhiteboard').then(function(map) {
+      attachLoggingToSyncMap(map);
+
+      // Sync any new drawn objects, here or remotely.
+      //
+      wireNewItemEventsBetweenMapAndCanvas(map, canvas);
+
+      // Load any the existing whiteboard data.
+      //
+      let readPage = (page) => {
+        for (let item of page.items) {
+          console.log("Twilio Sync: Loading item", item.key, item.value);
+          addRemoteItemToLocalCanvas(item, canvas)
+        };
+
+        if (page.hasNextPage)
+          page.nextPage().then(readPage);
+      }
+
+      map.getItems().then(readPage);
+   });
   })
-
-  canvas.on('object:modified', function (evt) {
-      console.log(evt);
-  });
-
-  //Get an access token for the current user, passing a device ID
-  //In browser-based apps, every tab is like its own unique device
-  //synchronizing state -- so we'll use a random UUID to identify
-  //this tab.
-  $.getJSON('/token', function (tokenResponse) {
-    //Initialize the Sync client
-    syncClient = new Twilio.Sync.Client(tokenResponse.token, { logLevel: 'info' });
-
-    //Let's pop a message on the screen to show that Sync is ready
-    $message.html('Sync initialized!');
-
-    //Now that Sync is active, lets enable our game board
-    $buttons.attr('disabled', false);
-
-    //This code will create and/or open a Sync document
-    //Note the use of promises
-    syncClient.map('theWhiteboard').then(function(map) {
-      //Lets store it in our global variable
-      map.on("itemAdded", function(e) { console.log('ItemAdded', e); });
-
-      //Initialize game board UI to current state (if it exists)
-      map.getItems().then(function(page) {
-        if (page.items.length == 0) {
-          //
-          // BOOTSTRAP
-          //
-          // create a rectangle object
-          let rect1 = new fabric.Rect({
-            id: "dog",
-            left: 100,
-            top: 100,
-            fill: 'red',
-            width: 20,
-            height: 20
-          });
-
-          // "add" rectangle onto canvas
-          canvas.add(rect1);
-          let firstRect = map.set(rect1.id, _.pick(rect1, ['top', 'left', 'height', 'width', 'fill']));
-
-          // create a rectangle object
-          let rect2 = new fabric.Rect({
-            id: "penis",
-            left: 200,
-            top: 100,
-            fill: 'red',
-            width: 20,
-            height: 20
-          });
-
-          // "add" rectangle onto canvas
-          canvas.add(rect2);
-
-                      // "add" rectangle onto canvas
-          canvas.add(rect2);
-          let secondRect = map.set(rect2.id, _.extend(_.pick(rect2, ['top', 'left', 'angle', 'height', 'width', 'fill', 'scaleX', 'scaleY']), {'type': 'rectangle'}));
-
-          Promise.all([firstRect, secondRect], function() {
-            configureSyncing(map, canvas);
-          })
-        } else {
-          _.map(page.items, function(item) {
-            canvas.add(new fabric.Rect(_.extend(item.value, {id: item.key})));
-          });
-
-          configureSyncing(map, canvas);
-        }
-      });
-    });
-  });
-
-  function configureSyncing(map, canvas) {
-    map.on('itemUpdated', function(it) {
-      let canvasItem = it.value
-      thisRect = canvas.getItemByName(it.key);
-
-      const animation = {
-        onChange: canvas.renderAll.bind(canvas),
-        duration: 500,
-        easing: fabric.util.ease.easeOutBounce
-      };
-      thisRect.animate('left', canvasItem.left, animation);
-      thisRect.animate('top', canvasItem.top, animation);
-      thisRect.animate('angle', canvasItem.angle, animation)
-      thisRect.animate('scaleX', canvasItem.scaleX, animation);
-      thisRect.animate('scaleY', canvasItem.scaleY, animation);
-    });
-
-    canvas.on('object:modified', function (evt) {
-      let newValue = _.pick(evt.target, ['top', 'left', 'angle', 'scaleX', 'scaleY']);
-      map.mutate(evt.target.id, function(rectangleInMap) {
-        return _.extend(rectangleInMap, newValue);
-      });
-    });
-  }
-
-  //Whenever a board button is clicked:
-  $buttons.on('click', function (e) {
-    //Toggle the value: X, O, or empty
-    toggleCellValue($(e.target));
-
-    //Update the document
-    var data = readGameBoardFromUserInterface();
-
-    //Send updated document to Sync
-    //This should trigger "updated" events on other clients
-    syncDoc.set(data);
-
-  });
-
-  //Toggle the value: X, O, or empty (&nbsp; for UI)
-  function toggleCellValue($cell) {
-    var cellValue = $cell.html();
-
-    if (cellValue === 'X') {
-      $cell.html('O');
-    } else if (cellValue === 'O') {
-      $cell.html('&nbsp;');
-    } else {
-      $cell.html('X');
-    }
-  }
-
-  //Read the state of the UI and create a new document
-  function readGameBoardFromUserInterface() {
-    var board = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ];
-
-    for (var row = 0; row < 3; row++) {
-      for (var col = 0; col < 3; col++) {
-        var selector = '[data-row="' + row + '"]' +
-          '[data-col="' + col + '"]';
-        board[row][col] = $(selector).html().replace('&nbsp;', '');
-      }
-    }
-
-    return {board: board};
-  }
-
-  //Update the buttons on the board to match our document
-  function updateUserInterface(data) {
-    for (var row = 0; row < 3; row++) {
-      for (var col = 0; col < 3; col++) {
-        var selector = '[data-row="' + row + '"]' +
-          '[data-col="' + col + '"]';
-        var cellValue = data.board[row][col];
-        $(selector).html(cellValue === '' ? '&nbsp;' : cellValue);
-      }
-    }
-  }
-
 });
+
+/**
+ * Given an initialized Sync Map and a Canvas to draw on, this method attaches
+ * event handlers that convert local drawing events into Sync data updates and
+ * vice-versa.
+ *
+ * @param map is an open Sync Map.
+ * @param canvas is a running fabric.js Canvas.
+ */
+function wireNewItemEventsBetweenMapAndCanvas(map, canvas) {
+
+  ///////////////////////////////
+  // Events incoming from Sync //
+  ///////////////////////////////
+
+  map.on('itemAdded', (it) => {
+    // Suppresses echo: a local object, now sync'd, doesn't need to be drawn again
+    //
+    if (canvas.getItemByName(it.key) == null)
+      addRemoteItemToLocalCanvas(it, canvas);
+  });
+
+  map.on('itemUpdated', (item) => {
+    updateLocalItemPerRemote(item.key, item.value, canvas);
+  });
+
+
+  ////////////////////////////////////
+  // Local Fabric.js Drawing Events //
+  ////////////////////////////////////
+
+  canvas.on('object:added', (evt) => {
+    // Suppresses echo: a remote object, now drawn locally, doesn't need to be Sync'd again.
+    //
+    const the_object = evt.target;
+    if (! the_object.id) {
+      the_object.set('id', newUuid());
+      map.set(the_object.id, _.extend(the_object.toObject(), {type: the_object.get('type')}));
+    }
+  });
+
+  canvas.on('object:modified', (evt) => {
+    let updateRemoteObject = (localObject) => {
+      let objectData = localObject.toJSON();
+      map.set(localObject.id, objectData);
+    }
+
+    if (evt.target.type != 'group') {
+      updateRemoteObject(evt.target)
+    } else {
+      //
+      // We don't synchronized groups (e.g. selection of multiple objects).
+
+      for (let it of evt.target.getObjects())
+        updateRemoteObject(ungroupedVersionOf(it));
+    }
+  });
+
+}
+
+//---------------------------------------------------
+// Log lines, to help us understand our application
+//---------------------------------------------------
+function attachLoggingToCanvas(canvas) {
+  canvas.on('object:added',    evt => console.log("Fabric.js:", evt.target.get('type'), "added",    evt));
+  canvas.on('object:modified', evt => console.log("Fabric.js:", evt.target.get('type'), "modified", evt));
+}
+
+function attachLoggingToSyncMap(map) {
+  map.on("itemAdded",   it => { console.log('Twilio Sync: New item', it.key, 'in Sync map', it.value); });
+  map.on("itemUpdated", it => { console.log('Twilio Sync: Map item', it.key, 'updated',     it.value); });
+}
+//---------------------------------------------------
+
+
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////// Code below is more about drawing than Syncing ///////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+
+function addRemoteItemToLocalCanvas(item, canvas) {
+  fabric.util.enlivenObjects([item.value], objects => {
+    for(let o of objects) {
+      o.set('id', item.key);
+      canvas.add(o);
+    };
+  });
+}
+
+function updateLocalItemPerRemote(itemName, remoteItemData, canvas) {
+  fabric.util.enlivenObjects([remoteItemData], ([remoteItem]) => {
+    let localDrawnItem = canvas.getItemByName(itemName);
+
+    const animatedProperties = ['left', 'top', 'scaleX', 'scaleY', 'angle', 'skewX', 'skewY'];
+    const animation = {
+      onChange: canvas.renderAll.bind(canvas),
+      duration: 500,
+      easing: fabric.util.ease.easeOutBounce
+    };
+
+    // Groups cause interesting aberrations when drawing. This covers many of them.
+    //
+    let transformedProperties = localDrawnItem.group
+      ? wellNamedMatrixComponentsFrom(
+        fabric.util.qrDecompose(
+          fabric.util.multiplyTransformMatrices(
+            remoteItem.calcTransformMatrix(),
+            fabric.util.invertTransform(localDrawnItem.group.calcTransformMatrix()), false)))
+      : remoteItemData;
+
+    localDrawnItem.animate(_.pick(transformedProperties, animatedProperties), animation);
+    localDrawnItem.set(_.omit(remoteItemData, animatedProperties));
+  });
+}
+
+/**
+ * This supremely useful utility function really should be part of fabric.js
+ * itself. But in the meantime, we'll monkey-patch it in.
+ */
+fabric.Canvas.prototype.getItemByName = function(name) {
+  var object = null,
+      objects = this.getObjects();
+
+  for (var i = 0, len = this.size(); i < len; i++) {
+    if (objects[i].id && objects[i].id === name) {
+      object = objects[i];
+      break;
+    }
+  }
+
+  return object;
+};
+
+function newUuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+       return v.toString(16);
+     });
+}
+
+function ungroupedVersionOf(object) {
+  // Fabric groups mean the contained objects are translated. This isn't useful for
+  // the other side.
+  //
+  let matrixComponents = fabric.util.qrDecompose(object.calcTransformMatrix());
+  return _.extend(_.clone(object), wellNamedMatrixComponentsFrom(matrixComponents));
+}
+
+function wellNamedMatrixComponentsFrom(rawTransformationMatrix) {
+  return _.omit(
+    _.extend(rawTransformationMatrix,
+      { left: rawTransformationMatrix.translateX,
+        top: rawTransformationMatrix.translateY}),
+    ['translateX', 'translateY']);
+}
+
+function attachPencilButtonToCanvas(canvas) {
+  canvas.freeDrawingBrush.width = 4;
+  const colors = ['red', 'black', 'green', 'blue', 'gray', 'brown', 'orange'];
+  canvas.freeDrawingBrush.color = colors[Math.floor(Math.random() * colors.length)];
+  canvas.isDrawingMode = $('#pencil').hasClass('active');
+
+  $('#pencil').click(() => {
+    let self = $('#pencil');
+    if (self.hasClass('active')) {
+      self.removeClass('active btn-success');
+      self.addClass('btn-default');
+      $("#pencil > span").text("Not Free-Drawing");
+      canvas.isDrawingMode = false;
+    } else {
+      self.removeClass('btn-default');
+      self.addClass('active btn-success');
+      $("#pencil > span").text("Free-Drawing…");
+      canvas.isDrawingMode = true;
+    }
+  });
+}
