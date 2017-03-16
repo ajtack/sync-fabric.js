6a0bf177f898cb2b550c86b4d97b7082ace38223
Andres Jaan Tack <andres.jaan.tack@eesti.ee>
2017-03-16 17:18:45 +0200

    Update to Sync SDK v0.5, removing endpoint-id (obviated). (#7)

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-require 'twilio-ruby'
 require 'sinatra'
 require 'sinatra/json'
 require 'dotenv'
 require 'faker'
+require 'twilio-ruby'
 
 # Load environment configuration
 Dotenv.load
@@ -14,14 +14,8 @@ end
 
 # Generate a token for use in our IP Messaging application
 get '/token' do
-  # Get the user-provided ID for the connecting device
-  device_id = params['device']
-
   # Create a random username for the client
   identity = Faker::Internet.user_name
-
-  # Create a unique ID for the currently connecting device
-  endpoint_id = "TwilioDemoApp:#{identity}:#{device_id}"
 
   # Create an Access Token for IP messaging usage
   token = Twilio::JWT::AccessToken.new ENV['TWILIO_ACCOUNT_SID'],
@@ -30,7 +24,6 @@ get '/token' do
   # Create IP Messaging grant for our token
   grant = Twilio::JWT::AccessToken::SyncGrant.new
   grant.service_sid = ENV['TWILIO_SYNC_SERVICE_SID']
-  grant.endpoint_id = endpoint_id
   token.add_grant grant
 
   # Generate the token and send to client
